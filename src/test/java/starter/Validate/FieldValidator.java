ad07dbf7227614cc396219ded4ad74fae3303ac7
JHRafacand <martincandilejo@hotmail.com>
2023-07-19 04:41:46 -0500

    Final de Pruebas, Hallazgos en Readme

--- src/test/java/starter/Validate/FieldValidator.java
+++ src/test/java/starter/Validate/FieldValidator.java
@@ -1,18 +1,19 @@
 package starter.Validate;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import  starter.stepdefinitions.DataValidationSteps;
+import starter.stepdefinitions.DataValidationSteps;
 import net.serenitybdd.core.Serenity;
 import org.openqa.selenium.WebElement;
 
+import static net.serenitybdd.core.Serenity.getDriver;
+
 public class FieldValidator {
-    public WebDriver driver1;
-    public void validateName(String enteredValue) {
-        System.out.println("Valor recibido Funcion: " + enteredValue);
-        WebElement campo = driver1.findElement(By.xpath("//input[@id='name']"));
+
+    public void validateName(WebElement campo, String enteredValue) {
         campo.sendKeys(enteredValue);
         String valorIngresado = campo.getAttribute("value");
-        if (!ValidationUtils.validateName(valorIngresado)) {
+        if (ValidationUtils.validateName(valorIngresado)) {
             Serenity.recordReportData().withTitle("Validación de campo").andContents("El campo debe contener solo letras.");
             throw new AssertionError("El campo debe contener solo letras.");
         } else {
@@ -20,9 +21,45 @@ public class FieldValidator {
         }
     }
 
-    public static class ValidationUtils{
-        public static boolean validateName(String name){
+    public void validateCredit(WebElement campo, String enteredValue) {
+        campo.sendKeys(enteredValue);
+        String valorIngresado = campo.getAttribute("value");
+        if (ValidationCredit.validateCredit(valorIngresado)) {
+            Serenity.recordReportData().withTitle("Validación de campo").andContents("El campo debe ser una Tarjeta Validad.");
+            throw new AssertionError("El campo debe ser una Tarjeta Validad.");
+        } else {
+            Serenity.recordReportData().withTitle("Validación de campo").andContents("El campo no es una Tarjeta Valida.");
+        }
+    }
+
+    public static class ValidationUtils {
+        public static boolean validateName(String name) {
             return name.matches("^[A-Za-z]+$");
+        }
+    }
+
+    public static class ValidationCredit {
+        public static boolean validateCredit(String name) {
+            return name.matches("^\\\\d{4}-\\\\d{4}-\\\\d{4}-\\\\d{4}$");
+        }
+    }
+
+    public void validateMounth(WebElement campo, String enteredValue) {
+        campo.sendKeys(enteredValue);
+        String valorIngresado = campo.getAttribute("value");
+        System.out.println("Valor recibido:" + valorIngresado);
+        try {
+            int valorIngresadoint = Integer.parseInt(valorIngresado);
+            System.out.println("Valor recibido Funcion: " + valorIngresado);
+            if (valorIngresadoint >= 1 && valorIngresadoint <= 12) {
+                Serenity.recordReportData().withTitle("Validación de campo").andContents("El campo contiene un Mes Válido.");
+            } else {
+                Serenity.recordReportData().withTitle("Validación de campo").andContents("El campo no es un Mes válido.");
+                throw new AssertionError("El campo no es un Mes válido.");
+            }
+        } catch (NumberFormatException e) {
+            Serenity.recordReportData().withTitle("Validación de campo").andContents("El campo no contiene un valor numérico válido.");
+            throw new AssertionError("El campo no contiene un valor numérico válido.");
         }
     }
 }
--- src/test/resources/features/search/joice.feature
+++ src/test/resources/features/search/joice.feature
@@ -1,16 +1,21 @@
-Feature: Compra DemoBlaze
+Feature: Elegir Productos DemoBlaze
 
-  Scenario: Compra Choise
+  Scenario: Elegir Productos DemoBlaze
     Given User I navigate to demoblaze
     And he I select a product
 
+Feature: Comprar Productos DemoBlaze
+
   Scenario: Validar datos
-    Given Ingreso el "JuanPerez" campo name
-    When Valido el campo nombre
-    #And Ingreso el "<Country>" campo country
-    #And Ingreso el "<Creditcard>" campo Credut Card
-    #And Ingreso el "<Month>" campo Month
-    #And Ingreso el "<Year>" campo Year
-    #When Validar Campos
-    #Then No debe mostar error*/
+    Given Ingreso el "Juan Perez" campo Name
+    And Ingreso el "Ecuador" campo Country
+    And Ingreso el "Quito" campo City
+    And Ingreso el campo "9876-5432-1098-7654" Credit Card
+    Then Valido el campo Name
+    Then Valido el campo country
+    Then Valido ingreso ciudad
+    Then Valido ingreso tarjeta
+    Then Ingreso Mes
+    Then Valido año 2023
+    Then Finalizar Compra
 
